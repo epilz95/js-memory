f88c277f9beb6470146be3b2b456a22ad31196ab
Eva-Maria Pilz <epilz@epilz-pc-4so>
2018-01-18 14:22:07 +0100

    Add html and css structure Add initial html and css structure
    Style cards
    
    Add headline and overlay
    
    Add motive shuffle function

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,8 @@ import 'normalize.css'
 // $FlowFixMe
 import '../sass/main.scss'
 
+import { MOTIFS } from './config'
+
 const double = (n: number): number => {
   return n * 2
 }
@@ -13,3 +15,35 @@ export function testMe (name: string) {
   console.log(double(12))
   return `Hi ${name}`
 }
+
+const motives = [ ...MOTIFS, ...MOTIFS ]
+
+const shuffleArray = (array) => {
+  let counter = array.length
+
+  while (counter > 0) {
+    let index = Math.floor(Math.random() * counter)
+
+    counter--
+
+    let temp = array[counter]
+    array[counter] = array[index]
+    array[index] = temp
+  }
+
+  return array
+}
+
+const shuffleCards = (motives: Array<any>) => {
+  shuffleArray(motives)
+
+  const cardFronts = document.querySelectorAll('.card--front')
+
+  cardFronts.forEach((card, i) => {
+    const currClass = motives[i]
+
+    card.classList.add(currClass)
+  })
+}
+
+shuffleCards(motives)
